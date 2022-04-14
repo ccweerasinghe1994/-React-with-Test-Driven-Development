016bae529c40564b4a2c1acdcc02b6b7377842ca
wchamara <chamara.weerasinghe@my.sliit.lk>
2022-04-14 11:54:37 +0530

    removing vite and moving to yarn

---
+++ test-driven-react/src/pages/SignUpPage.test.js
@@ -0,0 +1,7 @@
+import SignUpPage from "../pages/SignUpPage";
+import { render, screen } from "@testing-library/react";
+it("has header", () => {
+  render(<SignUpPage />);
+  const header = screen.queryByRole("heading", { name: "Sign Up" });
+  expect(header).toBeInTheDocument();
+});
