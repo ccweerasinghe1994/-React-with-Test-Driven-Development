2cee9c7e41a9457985d12bd29b61a3d3c3bd5485
wchamara <chamara.weerasinghe@my.sliit.lk>
2022-04-17 07:57:50 +0530

    adding axios,implementing submit method and testing to check the body

--- test-driven-react/src/pages/SignUpPage.jsx
+++ test-driven-react/src/pages/SignUpPage.jsx
@@ -1,17 +1,38 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const SignUpPage = () => {
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [disabled, setDisabled] = useState(true);
 
-  const onPasswordChange = (e) => {
-    if (e.target.id === "password") {
-      setPassword(e.target.value);
+  const onChange = (e) => {
+    const { value, id } = e.target;
+    console.log(value, id);
+    if (id === "password") {
+      setPassword(value);
     }
-    if (e.target.id === "repeat-password") {
-      setConfirmPassword(e.target.value);
+    if (id === "repeat-password") {
+      setConfirmPassword(value);
     }
+    if (id === "email") {
+      setEmail(value);
+    }
+    if (id === "username") {
+      setUser(value);
+    }
+  };
+
+  const onClick = (event) => {
+    event.preventDefault();
+    const body = {
+      username: user,
+      email,
+      password,
+    };
+    axios.post("http://localhost:5000/api/users/signup", body);
   };
 
   useEffect(() => {
@@ -25,18 +46,19 @@ const SignUpPage = () => {
       setDisabled(true);
     }
   }, [confirmPassword, confirmPassword.length, password, password.length]);
+
   return (
     <>
       <h1>Sign Up</h1>
       <label htmlFor="username">Username</label>
-      <input id="username" />
+      <input id="username" onChange={onChange} />
       <label htmlFor="email">E-mail</label>
-      <input id="email" />
+      <input id="email" onChange={onChange} />
       <label htmlFor="password">Password</label>
-      <input id="password" type={"password"} onChange={onPasswordChange} />
+      <input id="password" type={"password"} onChange={onChange} />
       <label htmlFor="repeat-password">Repeat Password</label>
-      <input type="password" id="repeat-password" onChange={onPasswordChange} />
-      <button disabled={disabled} type="submit">
+      <input type="password" id="repeat-password" onChange={onChange} />
+      <button disabled={disabled} type="submit" onClick={onClick}>
         Sign Up
       </button>
     </>
--- test-driven-react/src/pages/SignUpPage.test.js
+++ test-driven-react/src/pages/SignUpPage.test.js
@@ -1,6 +1,7 @@
 import SignUpPage from "../pages/SignUpPage";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import axios from "axios";
 describe("signup page", () => {
   describe("Layout", () => {
     it("has header", () => {
@@ -67,6 +68,29 @@ describe("signup page", () => {
       userEvent.type(passwordInput, "password");
       userEvent.type(repeatPasswordInput, "password");
       expect(button).not.toBeDisabled();
+    });
+
+    it("send username, password, email to the backend", () => {
+      render(<SignUpPage />);
+      const usernameInput = screen.getByLabelText("Username");
+      const emailInput = screen.getByLabelText("E-mail");
+      const passwordInput = screen.getByLabelText("Password");
+      const repeatPasswordInput = screen.getByLabelText("Repeat Password");
+      userEvent.type(usernameInput, "username");
+      userEvent.type(emailInput, "abc@gmail.com");
+      userEvent.type(passwordInput, "password");
+      userEvent.type(repeatPasswordInput, "password");
+      const button = screen.getByRole("button", { name: "Sign Up" });
+      const mockFn = jest.fn();
+      axios.post = mockFn;
+      userEvent.click(button);
+      const firstCallOfTheMockFunction = mockFn.mock.calls[0];
+      const body = firstCallOfTheMockFunction[1];
+      expect(body).toEqual({
+        username: "username",
+        email: "abc@gmail.com",
+        password: "password",
+      });
     });
   });
 });
